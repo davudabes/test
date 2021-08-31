ba36a4287ed8defa23e83a27951cc83ae20b040c
davudabes <83095515+davudabes@users.noreply.github.com>
2021-08-31 16:11:54 -0500

    Create rubic.js

---
+++ rubic.js
@@ -0,0 +1,21 @@
+<script defer type="text/javascript">
+    // describe widget configuration and saving to a global variable for future use
+    var configuration = {
+        from: 'ETH',
+        to: 'RBC',
+        fromChain: 'ETH',
+        toChain: 'ETH',
+        amount: 1,
+        iframe: 'flex',
+        hideSelectionFrom: false,
+        hideSelectionTo: true,
+        theme: 'dark',
+        background: '#28372e'
+    }
+
+    // prevent accidental changes to the object, for example, when re-creating a widget for another theme
+    Object.freeze(configuration);
+
+    // create widget
+    rubicWidget.init(configuration);
+</script>
